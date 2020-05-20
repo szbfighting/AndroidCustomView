5f4cfbf2f376fb3197ce8627519753cc86a2a57b
szbfighting <songzebin@xiyou3g.com>
2020-05-21 00:27:18 +0800

    完善了自定义属性

--- CustomView/app/src/main/java/com/song/customview/customview/InputNumberaView.java
+++ CustomView/app/src/main/java/com/song/customview/customview/InputNumberaView.java
@@ -2,8 +2,8 @@ package com.song.customview.customview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -18,7 +18,14 @@ public class InputNumberaView extends RelativeLayout {
     private EditText numberEdt;
     private View plusBtn;
     private OnNumberValueChangedListener listener = null;
+    private int max;
+    private int min;
+    private int step;
+    private int defaultValue;
+    private boolean disable;
+    private int btnBackground;
 
+    private static final String TAG = "InputNumberaView";
     public InputNumberaView(Context context) {
         this(context,null);
     }
@@ -30,42 +37,74 @@ public class InputNumberaView extends RelativeLayout {
     public InputNumberaView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         //获取相关属性
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.InputNumberView);
-        int max = typedArray.getInt(R.styleable.InputNumberView_max,0);
-        int min = typedArray.getInt(R.styleable.InputNumberView_min,0);
-        int step = typedArray.getInt(R.styleable.InputNumberView_step,0);
-        boolean disable = typedArray.getBoolean(R.styleable.InputNumberView_disable,false);
-        int btnBaackground = typedArray.getResourceId(R.styleable.InputNumberView_btnBackground, -1);
+        TypedArray typedArray = initAttr(context, attrs);
 
         //
         typedArray.recycle();
         initView(context);
         //设置事件
-        minusBtn = this.findViewById(R.id.minus);
-        numberEdt = this.findViewById(R.id.number);
-        plusBtn = this.findViewById(R.id.plus);
+
         setUpEvent();
 
+    }
+
+    private TypedArray initAttr(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.InputNumberView);
+        max = typedArray.getInt(R.styleable.InputNumberView_max,0);
+        min = typedArray.getInt(R.styleable.InputNumberView_min,0);
+        step = typedArray.getInt(R.styleable.InputNumberView_step,1);
+        defaultValue = typedArray.getInt(R.styleable.InputNumberView_defaultValue,0);
+
+        disable = typedArray.getBoolean(R.styleable.InputNumberView_disable,false);
+        btnBackground = typedArray.getResourceId(R.styleable.InputNumberView_btnBackground, -1);
+
+        Log.d(TAG, "max -- > " + max);
+        Log.d(TAG, "min -- > " + min);
+        Log.d(TAG, "step -- > " + step);
+        Log.d(TAG, "defaultValue -- > " + defaultValue);
+        Log.d(TAG, "disable -- > " + disable);
+        Log.d(TAG, "btnBackground -- > " + btnBackground);
+        return typedArray;
     }
 
     private void setUpEvent() {
         minusBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                setNumber(number-1);
+                plusBtn.setEnabled(true);
+
+                if (number-step<min){
+                    Log.d(TAG, "onClick: already min value ......");
+                    return;
+                }
+                setNumber(number-step);
+                if (number-step<min)
+                    v.setEnabled(false);
             }
         });
 
         plusBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                setNumber(number+1);
+                minusBtn.setEnabled(true);
+                if (number+step>max) {
+                    Log.d(TAG, "onClick: already max value ......");
+                    return;
+                }
+                setNumber(number+step);
+                if (number+step>max)
+                    v.setEnabled(false);
             }
         });
     }
 
     private void initView(Context context) {
         LayoutInflater.from(context).inflate(R.layout.input_number_view,this,true);
+        minusBtn = this.findViewById(R.id.minus);
+        numberEdt = this.findViewById(R.id.number);
+        plusBtn = this.findViewById(R.id.plus);
+        setDefaultValue(defaultValue);
+        setDisable(disable);
     }
 
     public int getNumber() {
@@ -85,5 +124,56 @@ public class InputNumberaView extends RelativeLayout {
 
     public void setOnNumberChangedListener(OnNumberValueChangedListener listener){
         this.listener = listener;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
+
+    public int getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(int defaultValue) {
+        this.defaultValue = defaultValue;
+        setNumber(defaultValue);
+    }
+
+    public boolean isDisable() {
+        return disable;
+    }
+
+    public void setDisable(boolean disable) {
+        this.disable = disable;
+        plusBtn.setEnabled(!disable);
+        minusBtn.setEnabled(!disable);
+    }
+
+    public int getBtnBackground() {
+        return btnBackground;
+    }
+
+    public void setBtnBackground(int btnBackground) {
+        this.btnBackground = btnBackground;
     }
 }
